a8f0c3be03f842c59be5cf3e9628d496b92d0553
Ananya <ananya@uoguelph.ca>
2021-08-14 03:45:30 -0400

    added login functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Header from './Header';
 import './Header.css';
 import Sidebar from './Sidebar';
+import Feed from './Feed'
+import { login, logout, selectUser } from './features/userSlice';
+import Login from './Login';
+import {auth} from './firebase';
 
 function App() {
+
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+   useEffect(()=>{
+    auth.onAuthStateChanged(userAuth =>{
+      if(userAuth){
+        //user is logged in
+        dispatch(login)
+      }else{
+        //user is logged out
+        dispatch(logout())
+      }
+    })
+  },[]);
+
   return (
     <div className="app">
       <Header/>
-      {/*App Body*/}
-      <div className="app-body">
-        <Sidebar/>
-        {/*Feed*/}
-        {/*widgets*/}
-      </div>
-    </div>
+
+      {!user ? (
+        <Login/>
+      ) : (
+        <div className="app-body">
+          <Sidebar/>
+          <Feed/>
+          {/*widgets*/}
+        </div>
+      )}
+    </div> 
   );
 }
 
