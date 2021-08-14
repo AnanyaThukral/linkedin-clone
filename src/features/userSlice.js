acb88a5c16ab7121137ed70e513144b56072f1c3
Ananya <ananya@uoguelph.ca>
2021-08-14 03:45:09 -0400

    setting redux

---
+++ src/features/userSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  user: null,
+  status: 'idle',
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.user += action.payload;
+    },
+    logout: (state) => {
+      state.user += null;
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+
+//pull the user from the the redux data layer
+//Selectors
+export const selectUser = (state) => state.user.user;
+
+export default userSlice.reducer;
